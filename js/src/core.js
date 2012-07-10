93f3ba338c47284ba5a61e9346215279c9da5447
U-Owner-PC\Owner <Owner@Owner-PC.(none)>
2012-07-10 14:35:32 -0400

    Added Fadein to View after Load

--- js/src/core.js
+++ js/src/core.js
@@ -2,9 +2,11 @@ var AwesomeTalker;
 
 (function(){
     AwesomeTalker = function(config) {
-      document.getElementById("LEFT_COLUMN").style.display = "block";
-      document.getElementById("RIGHT_COLUMN").style.display = "block";
       AwesomeSelector(config.CHARACTERS);
+
+      $("#LEFT_COLUMN").fadeIn();
+      $("#RIGHT_COLUMN").fadeIn();
+
 		  return AwesomeTalker;
     };
 })();
