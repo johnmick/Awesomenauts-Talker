fc736b38f274d923aa6848b881e5c3b0734e3127
johnmick <john.d.mick@gmail.com>
2012-07-10 22:47:50 -0400

    Added Temporary Phrase - Added Credits

--- js/src/awesomephrases.js
+++ js/src/awesomephrases.js
@@ -1,11 +1,26 @@
 var AwesomePhrases;
 
 (function(){
-  AwesomePhrases = function(config) {
+  var phrasesID = "PHRASES",
+      frag      = document.createDocumentFragment(),
+      container
+  ;
 
+  AwesomePhrases = function(config) {
+    container = document.getElementById(phrasesID);
   };
 
   AwesomePhrases.showPhrases = function(phrases) {
-    console.log(phrases);
+    for (var i=phrases.length-1; i > -1; i--)
+    {
+      var phrase = document.createElement("DIV");
+      phrase.className  = "PHRASE_BUTTON";
+      phrase.innerHTML  = phrases[i].TXT;
+      phrase.style.top  = phrases[i].Y + "px";
+      phrase.style.left = phrases[i].X + "px";
+      frag.appendChild(phrase);
+    }
+    $(container).empty();
+    container.appendChild(frag);
   };
 })();
--- js/src/core.js
+++ js/src/core.js
@@ -3,6 +3,7 @@ var AwesomeTalker;
 (function(){
     AwesomeTalker = function(config) {
       AwesomeSelector(config.CHARACTERS);
+      AwesomePhrases(config.CHARACTERS);
 
       $("#LEFT_COLUMN").fadeIn();
       $("#RIGHT_COLUMN").fadeIn();
