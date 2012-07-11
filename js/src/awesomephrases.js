a327a43d3b8787e638804be1f080e1665b153c58
johnmick <john.d.mick@gmail.com>
2012-07-10 23:45:10 -0400

    Improved Style for the Button - Improved Link Style - Buttons Contain Audio Data

--- js/src/awesomephrases.js
+++ js/src/awesomephrases.js
@@ -2,6 +2,9 @@ var AwesomePhrases;
 
 (function(){
   var phrasesID = "PHRASES",
+      normalColor = "#FFFFFF",
+      hoverColor  = "#FFFFD0",
+      downColor   = "#FFFFE0",
       frag      = document.createDocumentFragment(),
       container
   ;
@@ -14,10 +17,22 @@ var AwesomePhrases;
     for (var i=phrases.length-1; i > -1; i--)
     {
       var phrase = document.createElement("DIV");
+      $(phrase).data(phrases[i]);
       phrase.className  = "PHRASE_BUTTON";
       phrase.innerHTML  = phrases[i].TXT;
       phrase.style.top  = phrases[i].Y + "px";
       phrase.style.left = phrases[i].X + "px";
+      $(phrase).hover(
+        function() { this.style.color = hoverColor; }, // Hover On
+        function() { this.style.color = normalColor; this.style.fontWeight = "normal"; }  // Hover Off
+      );
+      $(phrase).mousedown( function(){ this.style.fontWeight = "bold"; this.style.color = downColor; } );
+      $(phrase).mouseup( function(){  this.style.fontWeight  = "normal"; this.style.color = normalColor; } );
+      $(phrase).click( function(){ 
+        this.style.fontWeight = "normal"; 
+        this.style.color = normalColor;
+        console.log($(this).data()); 
+      });
       frag.appendChild(phrase);
     }
     $(container).empty();
