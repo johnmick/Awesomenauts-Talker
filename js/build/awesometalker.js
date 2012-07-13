de435e571c89efbf37289f0d0efb0eb6f519fe47
johnmick <john.d.mick@gmail.com>
2012-07-12 22:30:02 -0400

    Sounds Test

--- js/build/awesometalker.js
+++ js/build/awesometalker.js
@@ -99,11 +99,26 @@ var AwesomeSounds;
   AwesomeSounds = function(config) {
     soundManager.url = "./swf/";
     soundManager.setup({
-      url:"./swf/"
+      url:"./swf/",
+      onready:function(){
+        loadSounds(config);
+      }
     });
-    console.log("Here");
-
   };
+
+  function loadSounds(config) {
+    console.log("SoundManager2 Loaded, Ready To Load Sounds");
+    console.log(config);
+
+    /*
+    var mySound = soundManager.createSound({
+      id: 'aSound',
+      url: '/path/to/an.mp3'
+      // onload: function() { console.log('sound loaded!', this); }
+    });
+    mySound.play();
+    */
+  }
 })();
 /*-----------------*\
 | MAIN Entry Point |
