513284f7701116a47f1ebca9169d209008d2346a
johnmick <john.d.mick@gmail.com>
2012-07-18 01:42:05 -0400

    Fixed corrupted jpgs, created two test pngs for portraits, updated selector code to manage hovering and toggle in JavaScript as opposed to browser based CSS caching

--- js/src/awesomeselector.js
+++ js/src/awesomeselector.js
@@ -7,15 +7,36 @@ var AwesomeSelector;
       frag       = document.createDocumentFragment(),
       container,
       portrait,
-      currentCharacter
+      currentCharacter = "ronimo",
+      portraits = {}
   ;
 
     AwesomeSelector = function(characters) {
       container = document.getElementById(selectorID);
       portrait  = document.getElementById(portraitID);
 
+      var ronimo = document.createElement("IMG");
+      ronimo.id = "RONIMO_PORTRAIT";
+      ronimo.src = "./images/logos/ronimo.png";
+      ronimo.style.width = "305px";
+      ronimo.style.height = "425px";
+      portrait.appendChild(ronimo);
+      portraits.ronimo = ronimo;
+
       for (var character in characters)
       {
+        function createPortraitImg() {
+          var port = document.createElement("IMG");
+          port.src = characters[character].PORTRAIT_SRC;
+          port.id  = character + "_PORTRAIT";
+          port.style.width = "305px";
+          port.style.height = "425px";
+          port.style.display = "none";
+          portrait.appendChild(port);
+          return port;
+        }
+        portraits[character] = createPortraitImg();
+
         var preCacheImage = new Image();
         preCacheImage.src = characters[character].PORTRAIT_SRC;
 
@@ -23,19 +44,30 @@ var AwesomeSelector;
         icon.className = "AWESOME_ICON";
         icon.style.width           = iconSize.x;
         icon.style.height          = iconSize.y;
-        icon.style.backgroundImage = "url('" + characters[character].ICON_SRC + "')";
         $(icon).data(characters[character]);
         $(icon).data("Character", character);
         $(icon).click(characterSelected);
-        $(icon).hover(
-          function() {
-            AwesomeSounds.play("UI", "UI_ICON_MOUSE_OVER");
-            this.style.backgroundImage = "url('" + $(this).data().ICON_SRC_ALT + "')";
-          },
-          function() {
-            this.style.backgroundImage = "url('" + $(this).data().ICON_SRC + "')"; 
-          }
-        );
+
+        (function(){
+          var normal = document.createElement("IMG");
+          var hover  = document.createElement("IMG");
+          hover.style.display = "none";
+          normal.src = characters[character].ICON_SRC;
+          hover.src = characters[character].ICON_SRC_ALT;
+          $(icon).hover(
+            function() {
+              AwesomeSounds.play("UI", "UI_ICON_MOUSE_OVER");
+              hover.style.display = "block";
+              normal.style.display = "none";
+            },
+            function() {
+              hover.style.display = "none";
+              normal.style.display = "block";
+            }
+          );
+          icon.appendChild(normal);
+          icon.appendChild(hover);
+        })();
         frag.appendChild(icon);
       }
       container.appendChild(frag);
@@ -53,11 +85,12 @@ var AwesomeSelector;
       if (data.Character !== currentCharacter)
       {
         AwesomeSounds.play("UI", "UI_ICON_CLICK");
-        portrait.style.backgroundImage = "url('" + $(this).data().PORTRAIT_SRC + "')";
         AwesomePhrases.showPhrases(data.Character, data.PHRASES);
         AwesomeVCR.show();
         AwesomeVCR.reset();
+        portraits[currentCharacter].style.display = "none";
         currentCharacter = data.Character;
+        portraits[currentCharacter].style.display = "block";
       }
     }
 })();
