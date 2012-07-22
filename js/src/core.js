4f19e4272a0cccbde1aa59e04c3f4214f233e929
johnmick <john.d.mick@gmail.com>
2012-07-22 04:06:39 -0400

    Added Konami Code Easter Egg for Draggable Phrases

--- js/src/core.js
+++ js/src/core.js
@@ -8,6 +8,29 @@ var AwesomeTalker;
       AwesomeTalker.AwesomeSounds   = AwesomeSounds(config);
       AwesomeTalker.AwesomeVCR      = AwesomeVCR(config);
       AwesomeTalker.AwesomeMessage  = AwesomeMessage(config);
+
+      var konami = [38,38,40,40,37,39,66,65];
+      var codeIndex = 0;
+      $(window).keydown(function(e){
+        if (konami[codeIndex] == e.keyCode)
+        {
+          if (codeIndex === 7)
+          {
+            $(".PHRASE_BUTTON").draggable();
+            AwesomeSounds.play("UI", "UI_ICON_CLICK");
+            codeIndex = 0;
+          }
+          else
+          {
+            codeIndex++;
+          }
+        }
+        else
+        {
+          codeIndex = 0;
+        }
+        return true;
+      });
 		  return AwesomeTalker;
     };
 })();
