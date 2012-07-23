45d96e3b07413e6aa3dde1af79e038b04e4f0458
johnmick <john.d.mick@gmail.com>
2012-07-22 21:00:57 -0400

    Updated Portraits - Made Graphics for VCR - Fixed Konami Sequence

--- js/src/core.js
+++ js/src/core.js
@@ -9,12 +9,12 @@ var AwesomeTalker;
       AwesomeTalker.AwesomeVCR      = AwesomeVCR(config);
       AwesomeTalker.AwesomeMessage  = AwesomeMessage(config);
 
-      var konami = [38,38,40,40,37,39,66,65];
+      var konami = [38,38,40,40,37,39,37,39,66,65];
       var codeIndex = 0;
       $(window).keydown(function(e){
         if (konami[codeIndex] == e.keyCode)
         {
-          if (codeIndex === 7)
+          if (codeIndex === 9)
           {
             $(".PHRASE_BUTTON").draggable();
             AwesomeSounds.play("UI", "UI_ICON_CLICK");
